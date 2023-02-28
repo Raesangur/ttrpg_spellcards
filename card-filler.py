0d6684d24a206972297bf44ad3ea13f43bc31fec
Raesangur <pascalpinguin@gmail.com>
2023-02-27 23:14:06 -0500

    reading school of magic from pdf

--- card-filler.py
+++ card-filler.py
@@ -1,51 +1,33 @@
 from pdf2image import convert_from_path
+from PyPDF2 import PdfReader
 import cv2
 import numpy as np
 import sys
 import tkinter as tk
 import tkinter.filedialog as fd
 
-filez = []
-backs = False
-if len(sys.argv) == 1:
-    # Open file dialog and get 1 to n file paths
-    root = tk.Tk()
-    root.withdraw()
-    filez = fd.askopenfilenames(parent = root, title = "Cards to load (up to 4)", filetypes=[("Portable Document Format", "*.pdf")])
+# Open file dialog and get 1 to n file paths
+root = tk.Tk()
+root.withdraw()
+filez = fd.askopenfilenames(parent = root, title = "Cards to load (up to 4)", filetypes=[("Portable Document Format", "*.pdf")])
 
-    if (len(filez) > 4) or (len(filez) == 0):
-        print("Max 4 cards at a time")
-        exit()
-else:
-    cardBacks = {
-    "abjuration"    : "./backs/back_abjuration.png",
-    "conjuration"   : "./backs/back_conjuration.png",
-    "divination"    : "./backs/back_divination.png",
-    "enchantment"   : "./backs/back_enchantment.png",
-    "evocation"     : "./backs/back_evocation.png",
-    "illusion"      : "./backs/back_illusion.png",
-    "necromancy"    : "./backs/back_necromancy.png",
-    "transmutation" : "./backs/back_transmutation.png",
+if (len(filez) > 4) or (len(filez) == 0):
+    print("Max 4 cards at a time")
+    exit()
 
-    "abj"     : "./backs/back_abjuration.png",
-    "abjur"   : "./backs/back_abjuration.png",
-    "con"     : "./backs/back_conjuration.png",
-    "conjur"  : "./backs/back_conjuration.png",
-    "div"     : "./backs/back_divination.png",
-    "divi"    : "./backs/back_divination.png",
-    "divin"   : "./backs/back_divination.png",
-    "ench"    : "./backs/back_enchantment.png",
-    "enchant" : "./backs/back_enchantment.png",
-    "evo"     : "./backs/back_evocation.png",
-    "evoca"   : "./backs/back_evocation.png",
-    "illu"    : "./backs/back_illusion.png",
-    "nec"     : "./backs/back_necromancy.png",
-    "necro"   : "./backs/back_necromancy.png",
-    "trans"   : "./backs/back_transmutation.png",
-    }
-    backs = True
-    for arg in sys.argv[1:]:
-        filez.append(cardBacks[arg])
+cardBacks = {
+"abjuration"    : "./backs/back_abjuration.png",
+"conjuration"   : "./backs/back_conjuration.png",
+"divination"    : "./backs/back_divination.png",
+"enchantment"   : "./backs/back_enchantment.png",
+"evocation"     : "./backs/back_evocation.png",
+"illusion"      : "./backs/back_illusion.png",
+"necromancy"    : "./backs/back_necromancy.png",
+"transmutation" : "./backs/back_transmutation.png",
+}
+
+schoolList = ["Abjuration", "Conjuration", "Divination", "Enchantment", "Evocation", "Illusion", "Necromancy", "Transmutation", \
+              "abjuration", "conjuration", "divination", "enchantment", "evocation", "illusion", "necromancy", "transmutation"]
 
 #
 cardWidth   = 900
@@ -57,36 +39,34 @@ firstCardY  = 100
 
 # Create blank image of the right size
 blankImage = np.ones((paperHeight, paperWidth, 3), dtype=np.uint8)
+backImage  = np.ones((paperHeight, paperWidth, 3), dtype=np.uint8)
 blankImage.fill(255)
-
+backImage.fill(255)
 
 # Add cards to the image
 for i in range(len(filez)):
-    # Read image and convert PIL image to cv2 image if PDF
+    # Extract image from PDF, convert to cv2 image and scale image
     filename = filez[i]
-    fileExtension = "." + filez[i].split(".")[-1]
-    img = ""
-    if fileExtension == ".png":
-        img = cv2.imread(filename)
-    elif fileExtension == ".pdf":
-        img = convert_from_path(filename)[0].convert('RGB')
-        img = np.array(img)[:, :, ::-1].copy()
-    else:
-        img = "error - unsupported file extension"
-
-    # Scale image to 900 x 1500
+    img = convert_from_path(filename)[0].convert('RGB')
+    img = np.array(img)[:, :, ::-1].copy()
     img = cv2.resize(img, (cardWidth, cardHeight), interpolation = cv2.INTER_AREA)
 
+    # Extract school of magic picture
+    text       = PdfReader(filename).pages[0].extract_text()
+    school     = [word for word in schoolList if word in text]
+    schoolBack = cardBacks[school[0].lower()]
+    back       = cv2.imread(schoolBack)
+
     # Calculate offsets
-    y = i // 2
-    x = 0
-    if backs == False:
-        x = i %  2
-    else:
-        x = (i + 1) % 2
-    xOff = firstCardX * (x + 1) + cardWidth  * x
-    yOff = firstCardY * (y + 1) + cardHeight * y
-    blankImage[yOff:yOff + img.shape[0], xOff:xOff + img.shape[1]] = img
+    y  = i // 2
+    x  = i %  2
+    xb = (i + 1) % 2
+
+    xOff  = firstCardX * (x + 1)  + cardWidth  * x
+    xOffb = firstCardX * (xb + 1) + cardWidth  * xb
+    yOff  = firstCardY * (y + 1)  + cardHeight * y
+    blankImage[yOff:yOff + img.shape[0],  xOff :xOff  + img.shape[1]]  = img
+    backImage [yOff:yOff + back.shape[0], xOffb:xOffb + back.shape[1]] = back
 
 # Add cutting guides
 lineThickness = 1
@@ -100,6 +80,11 @@ for i in [0, 1]:
     cv2.line(blankImage, (0, yOff2), (paperWidth, yOff2),  lineColor, thickness=lineThickness)
     cv2.line(blankImage, (xOff1, 0), (xOff1, paperHeight), lineColor, thickness=lineThickness)
     cv2.line(blankImage, (xOff2, 0), (xOff2, paperHeight), lineColor, thickness=lineThickness)
+    cv2.line(backImage,  (0, yOff1), (paperWidth, yOff1),  lineColor, thickness=lineThickness)
+    cv2.line(backImage,  (0, yOff2), (paperWidth, yOff2),  lineColor, thickness=lineThickness)
+    cv2.line(backImage,  (xOff1, 0), (xOff1, paperHeight), lineColor, thickness=lineThickness)
+    cv2.line(backImage,  (xOff2, 0), (xOff2, paperHeight), lineColor, thickness=lineThickness)
 
 # Save image
-cv2.imwrite("newcards.png", blankImage)
+cv2.imwrite("newcards.png",  blankImage)
+cv2.imwrite("cardbacks.png", backImage)

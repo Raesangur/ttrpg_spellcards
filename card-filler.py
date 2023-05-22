11f12cb91a2fbf456e0c6f5ee245b9283665b180
Raesangur <pascalpinguin@gmail.com>
2023-05-21 23:51:25 -0400

    generating hero card pdf automatically from json file

--- card-filler.py
+++ card-filler.py
@@ -25,10 +25,24 @@ def create_images(files):
         img = cv2.resize(img, (cardWidth, cardHeight), interpolation = cv2.INTER_AREA)
 
         # Extract school of magic picture
-        text       = PdfReader(filename).pages[0].extract_text()
-        school     = [word for word in schoolList if word in text]
-        schoolBack = cardBacks[school[0].lower()]
-        back       = cv2.imread(schoolBack)
+        text        = PdfReader(filename).pages[0].extract_text()
+        school      = [word for word in schoolList if word in text]
+        schoolBack  = cardBacks[school[0].lower()]
+        schoolFront = cardFronts[school[0].lower()]
+        back        = cv2.imread(schoolBack)
+        front       = cv2.imread(schoolFront, cv2.IMREAD_UNCHANGED)
+
+        alpha = np.ones(img.shape, dtype=np.uint8)
+        alpha[:, :, 0] = front[:, :, 3]
+        alpha[:, :, 1] = front[:, :, 3]
+        alpha[:, :, 2] = front[:, :, 3]
+
+        alpha = alpha.astype(float)/255
+        front = front.astype(float)
+        img   = img.astype(float)
+
+        front = cv2.multiply(alpha, front[:, :, :3])
+        img   = cv2.multiply(1.0 - alpha, img)
 
         # Calculate offsets
         y  = i // 2
@@ -38,7 +52,7 @@ def create_images(files):
         xOff  = firstCardX * (x + 1)  + cardWidth  * x
         xOffb = firstCardX * (xb + 1) + cardWidth  * xb
         yOff  = firstCardY * (y + 1)  + cardHeight * y
-        blankImage[yOff:yOff + img.shape[0],  xOff :xOff  + img.shape[1]]  = img
+        blankImage[yOff:yOff + img.shape[0],  xOff :xOff  + img.shape[1]]  = cv2.add(img, front)
         backImage [yOff:yOff + back.shape[0], xOffb:xOffb + back.shape[1]] = back
 
     # Add cutting guides
@@ -74,10 +88,22 @@ cardBacks = {
 "illusion"      : "./backs/back_illusion.png",
 "necromancy"    : "./backs/back_necromancy.png",
 "transmutation" : "./backs/back_transmutation.png",
+"hero"          : "./backs/back_hero.png"
+}
+cardFronts = {
+"abjuration"    : "./backs/front_abjuration.png",
+"conjuration"   : "./backs/front_conjuration.png",
+"divination"    : "./backs/front_divination.png",
+"enchantment"   : "./backs/front_enchantment.png",
+"evocation"     : "./backs/front_evocation.png",
+"illusion"      : "./backs/front_illusion.png",
+"necromancy"    : "./backs/front_necromancy.png",
+"transmutation" : "./backs/front_transmutation.png",
+"hero"          : "./backs/front_hero.png"
 }
 
-schoolList = ["Abjuration", "Conjuration", "Divination", "Enchantment", "Evocation", "Illusion", "Necromancy", "Transmutation", \
-              "abjuration", "conjuration", "divination", "enchantment", "evocation", "illusion", "necromancy", "transmutation"]
+schoolList = ["Abjuration", "Conjuration", "Divination", "Enchantment", "Evocation", "Illusion", "Necromancy", "Transmutation", "Hero", \
+              "abjuration", "conjuration", "divination", "enchantment", "evocation", "illusion", "necromancy", "transmutation", "hero"]
 
 #
 cardWidth   = 900
@@ -94,6 +120,4 @@ for i in range(math.ceil(len(filez) / 4)):
     images.append(Image.fromarray(backImage[...,::-1]))
 
 images[0].save("output.pdf", "PDF", resolutionm=100.0, save_all=True, append_images=images[1:])
-# Save image
-#cv2.imwrite("newcards.png",  frontImage)
-#cv2.imwrite("cardbacks.png", backImage)
+
---
+++ card-generator.py
@@ -0,0 +1,35 @@
+import json
+import os
+import re
+import subprocess
+
+with open("hero-template.tex", 'r') as inputF:
+    template = inputF.read()
+
+    with open("hero-cards.json", 'r') as cards:
+        cards_dict = json.load(cards)
+
+        for card in cards_dict["cards"]:
+            title = card["hero-title"].replace(' ', '_')
+            spacing  = "10mm" if len(title) < 16 else "16mm" if len(title) < 32 else "20mm"
+            spacing2 = "3mm"  if len(title) < 16 else "9mm"  if len(title) < 32 else "13mm"
+
+            text = template.replace("hero-title", card["hero-title"])
+            text = text.replace("hero-type", card["hero-type"])
+            text = text.replace("hero-trigger", card["hero-trigger"])
+            text = text.replace("hero-description", card["hero-description"])
+            text = text.replace("hero-fortune", "This is a Fortune effect" if "Fortune" in card["hero-type"] else "")
+            #text = text.replace("hero-spacing-secondary", "2mm")
+            text = text.replace("hero-spacing-secondary", spacing2)
+            text = text.replace("hero-spacing",  spacing)
+
+            outputF = open("output/" + title + ".tex", 'w')
+            outputF.write(text)
+            outputF.close()
+
+            subprocess.call("pdflatex output/" + title + ".tex -output-directory=output -job-name=" + title)
+
+files = os.listdir("output")
+files = [f for f in files if ".pdf" not in f]
+for f in files:
+    os.remove("output/" + f)

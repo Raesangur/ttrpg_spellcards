56e1dd6d395f7dcb200373a38bf6583749f9a9c4
Raesangur <pascalemmanuel.lachance@gmail.com>
2024-02-10 15:59:02 -0500

    editing and adding to character sheets

--- character-generator.py
+++ character-generator.py
@@ -9,14 +9,40 @@ root = tk.Tk()
 root.withdraw()
 filez = fd.askopenfilenames(parent = root, title = "Character sheets to load", filetypes=[("Portable Document Format", "*.pdf")])
 
+# Iterate over character sheets
 for cs in filez:
     characterName = Path(cs).stem
     print(characterName)
     newName = cs.replace(".pdf", "-new.pdf")
 
     with open("character-sheets/" + characterName + ".json", 'r') as charJson:
+        # Load values from JSON
         charDict = json.load(charJson)
         print(charDict)
     
-        fillpdfs.get_form_fields(cs)
-        fillpdfs.write_fillable_pdf(cs, newName, data_dict)
+        # Get values currently in PDF
+        ogCharDict = fillpdfs.get_form_fields(cs)
+        # print(ogCharDict)
+
+        # Iterate over changes
+        newCharDict = {}
+        try:
+            for val in charDict["Changes"]:
+                # print(val)
+                key = list(val.keys())[1]
+                print(key)
+                if val["Mode"] == "Edit":
+                    newCharDict[key] = val[key]
+                elif val["Mode"] == "Add":
+                    newCharDict[key] = str(int(ogCharDict[key]) + int(val[key]))
+                else:
+                    printf("Invalid value " + val["Mode"])
+
+        except:
+            print("Invalid JSON (exiting):")
+            print(charDict)
+            exit()
+
+        # Save copy of the character sheet with new values
+        print(newCharDict)
+        fillpdfs.write_fillable_pdf(cs, newName, newCharDict)
